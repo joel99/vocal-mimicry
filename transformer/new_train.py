375882ab319fcd29023ca6189d0ec7d3a2dc6aac
Arda Pekis <arda@pek.is>
2019-04-18 18:10:49 -0400

    Fix criterion

--- transformer/new_train.py
+++ transformer/new_train.py
@@ -5,13 +5,14 @@ from torch import nn, optim
 
 def evaluate(model, val_dset, config=config):
     model.eval()
+    criterion = nn.BCELoss()
 
     total_loss = 0
     for data, style in tqdm(val_dset, desc="Generator evaluation."):
         data, style = data.to(model.device), style.to(model.device)
         pred = model(data, style)
         mel, is_voice, content, identity = pred
-        loss = nn.BCELoss(is_voice, 1) + nn.BCELoss(content, 1) + nn.BCELoss(identity, 1)
+        loss = criterion(is_voice, 1) + criterion(content, 1) + criterion(identity, 1)
         total_loss += loss
     print("Validation Loss: {}".format(total_loss / len(val_dset)))
     return total_loss / len(val_dset)
@@ -20,6 +21,7 @@ def evaluate(model, val_dset, config=config):
 def train(model, optimizer, train_dset, config=config):
     # Turn on training mode which enables dropout.
     model.train()
+    criterion = nn.BCELoss()
 
     pbar = tqdm(train_dset, desc="Generator training", unit="batch")
     for data, style in pbar:
@@ -27,7 +29,7 @@ def train(model, optimizer, train_dset, config=config):
         model.zero_grad()
         pred = model(data, style)
         mel, is_voice, content, identity = pred
-        loss = nn.BCELoss(is_voice, 1) + nn.BCELoss(content, 1) + nn.BCELoss(identity, 1)
+        loss = criterion(is_voice, 1) + criterion(content, 1) + criterion(identity, 1)
         loss.backward()
         optimizer.step()
         pbar.set_postfix({"loss": loss})
