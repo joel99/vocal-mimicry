aed5ee5aab1b1849d72524620be10729c303a221
Arda Pekis <ardapekis@gmail.com>
2019-04-25 13:58:57 -0400

    Person 225 is okay after all

--- dataset.py
+++ dataset.py
@@ -108,8 +108,7 @@ class VCTK_Wrapper:
     MAX_NUM_PEOPLE = 150
     MAX_NUM_SAMPLES = 300
     # For whatever reason, the ID of the first person is actually 225
-    # Person 225 sucks
-    STARTING_ID = 226
+    STARTING_ID = 225
 
     def __init__(self, embedder, num_people, num_samples,
                  mel_root, device):
