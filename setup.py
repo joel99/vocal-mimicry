f4fcbe37c37aecc418929f9137b27b6ff93a0590
Arda Pekis <ardapekis@gmail.com>
2019-04-25 13:59:47 -0400

    Update config and setup file

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name='vocal_mimicry',
     version='0.1.0',
     url='https://github.com/joel99/vocal-mimicry',
-    description='Code for Few-Shot Voice Transfer'
+    description='Code for Few-Shot Voice Transfer',
+    packages=['embedding', 'transformer']
 )
