0171005ec0e810e07c8532918d8f0ab5ea2e4609
Joel Ye <joelye9@gmail.com>
2019-04-30 14:02:10 -0400

    remove other items

--- data/setup_training.sh
+++
@@ -1,6 +0,0 @@
-# Script for fetching data and models necessary for training
-wget -O raw/VCTK_corpus.zip https://datashare.is.ed.ac.uk/bitstream/handle/10283/2651/VCTK-Corpus.zip?sequence=2&isAllowed=y
-cd raw
-unzip VCTK_corpus.zip
-rm VCTK_corpus.zip
-cd ..
--- poster/README.md
+++
@@ -1,3 +0,0 @@
-DON'T EDIT HERE, do it on the overleaf. This is just a backup in case csomething goes horribly wrong with overleaf.
-
-ONLY COMMIT UPDATE WITH VERSIONS WHICH COMPILE WITHOUT ERRORS.
